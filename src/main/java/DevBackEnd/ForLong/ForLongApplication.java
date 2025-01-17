3bf1fd404558e3bd5622c58d08678cd2daa10cf1
LeeHyunWoo02 <hyoun0824@naver.com>
2025-01-17 14:00:15 +0900

    초기 세팅

---
+++ src/main/java/DevBackEnd/ForLong/ForLongApplication.java
@@ -0,0 +1,13 @@
+package DevBackEnd.ForLong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForLongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForLongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DevBackEnd/ForLong/ForLongApplicationTests.java
@@ -0,0 +1,13 @@
+package DevBackEnd.ForLong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForLongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
