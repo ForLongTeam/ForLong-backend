d48934444d2d37ff73675d1ad3b0e3b9bbc4e906
LeeHyunWoo <hyoun0824@naver.com>
2025-01-25 00:52:39 +0900

    [docs] : api 명세서 문서화를 위한 로직 추가1

--- src/main/java/DevBackEnd/ForLong/Controller/UserController.java
+++ src/main/java/DevBackEnd/ForLong/Controller/UserController.java
@@ -8,10 +8,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     /**
