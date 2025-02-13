1403a59149559346c25664be4de51ff872ceb967
LeeHyunWoo02 <hyoun0824@naver.com>
2025-02-13 22:31:55 +0900

    docs : 개인정보 조회 몽록 추가

--- src/main/java/DevBackEnd/ForLong/Dto/FindUserDTO.java
+++ src/main/java/DevBackEnd/ForLong/Dto/FindUserDTO.java
@@ -12,15 +12,23 @@ import java.util.stream.Collectors;
 @Setter
 public class FindUserDTO {
 
+    private Long userId;
     private String loginid;
     private String nickname;
     private String email;
+    private String provider;
+    private String providerId;
+    private String role;
     private List<String> pets;
 
     public FindUserDTO(User user){
         this.loginid = user.getLoginId();
         this.nickname = user.getNickname();
         this.email = user.getEmail();
+        this.provider = user.getProvider();
+        this.providerId = user.getProviderId();
+        this.role = user.getRole();
+        this.userId = user.getId();
         this.pets = user.getPets().stream()
                 .map(Pet::getPet_name)
                 .collect(Collectors.toList());
