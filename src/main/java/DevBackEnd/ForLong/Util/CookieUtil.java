96379ff803090e2deb4ad1dd3b84928c7a5790f9
LeeHyunWoo02 <hyoun0824@naver.com>
2025-02-13 16:13:23 +0900

    docs : Cookie 설정 변경

--- src/main/java/DevBackEnd/ForLong/Util/CookieUtil.java
+++ src/main/java/DevBackEnd/ForLong/Util/CookieUtil.java
@@ -10,9 +10,9 @@ public class CookieUtil {
 
         Cookie cookie = new Cookie(key, value);
         cookie.setMaxAge(60*60); // 쿠키가 살아있을 시간
-        //cookie.setSecure(true);  //https 일 경우 주석 삭제
+        cookie.setSecure(false);  //https 일 경우 주석 삭제
         cookie.setPath("/");
-        cookie.setHttpOnly(true);
+        cookie.setHttpOnly(false);
 
         return cookie;
     }
