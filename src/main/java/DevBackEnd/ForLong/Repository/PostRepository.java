d2ce286d0a23c00ae76361d7fc5622d302dec0de
LeeHyunWoo02 <hyoun0824@naver.com>
2025-02-13 22:27:48 +0900

    docs : 게시물 상세보기 관련 DTO 추가

--- src/main/java/DevBackEnd/ForLong/Repository/PostRepository.java
+++ src/main/java/DevBackEnd/ForLong/Repository/PostRepository.java
@@ -1,6 +1,8 @@
 package DevBackEnd.ForLong.Repository;
 
 import DevBackEnd.ForLong.Entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Post save(Post post);
-
+    Page<Post> findAllByOrderByIdDesc(Pageable pageable);
 }
