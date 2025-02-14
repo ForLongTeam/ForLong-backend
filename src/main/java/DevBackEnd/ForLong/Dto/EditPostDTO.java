69a0e63df6d177878343fa3465880450f09e51b2
LeeHyunWoo02 <hyoun0824@naver.com>
2025-02-15 00:26:32 +0900

    feat : 게시물 수정, 조회 ,삭제 기능 추가

---
+++ src/main/java/DevBackEnd/ForLong/Dto/EditPostDTO.java
@@ -0,0 +1,13 @@
+package DevBackEnd.ForLong.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EditPostDTO {
+
+    private String title;
+    private String content;
+
+}
