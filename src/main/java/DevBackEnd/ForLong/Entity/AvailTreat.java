5a4428962d4846a9984b5efe51f502501a0c508b
LeeHyunWoo02 <hyoun0824@naver.com>
2025-02-16 01:33:39 +0900

    feat:진료가능 테이블 추가

---
+++ src/main/java/DevBackEnd/ForLong/Entity/AvailTreat.java
@@ -0,0 +1,7 @@
+package DevBackEnd.ForLong.Entity;
+
+public enum AvailTreat {
+    정형외과,
+    내과,
+    안과;
+}
---
+++ src/main/java/DevBackEnd/ForLong/Entity/Spiece.java
@@ -0,0 +1,9 @@
+package DevBackEnd.ForLong.Entity;
+
+public enum Spiece {
+    포유류,
+    설치류,
+    어류,
+    파충류,
+    조류;
+}
